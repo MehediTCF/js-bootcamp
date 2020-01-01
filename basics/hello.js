47fcf66dc5ae554ba0d67234eac4880c6bac7cfa
Mehedi Zamadar <meheditcf@gmail.com>
2020-01-02 03:11:49 +0600

    'variables'

--- basics/hello.js
+++ basics/hello.js
@@ -1,3 +1,15 @@
 let name = 'Hi There'
 
 console.log(name)
+
+let isRaining = true
+
+console.log(isRaining)
+
+let temp = 52
+let iss = temp === 52
+console.log(iss)
+
+if (temp <= 53) {
+    console.log('Raining!')
+}
