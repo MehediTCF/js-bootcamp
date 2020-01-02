563043a106d1ca59d1c4230bad19ae449a50f2d8
Mehedi Zamadar <meheditcf@gmail.com>
2020-01-03 03:09:13 +0600

    js functions basics

---
+++ basics/functions-101.js
@@ -0,0 +1,34 @@
+//basic functions
+let greatUser = function (num) {
+    console.log('Welcome!')
+    let result = num * num
+    console.log(result)
+}
+
+greatUser(5)
+
+//undefined for functions arguments
+let square = function (num) {
+    console.log(num)
+
+}
+
+let another = square()
+console.log(another)
+
+//with multiple arguments
+let add = function (a, b, c) {
+    return a + b + c;
+}
+
+let res = add(4, 5, 6)
+console.log(res)
+
+//challenge-1
+let tipcal = function (bill, per = 0.2) {
+    let tip = bill * per
+    return tip
+}
+
+let res1 = tipcal(100, 0.5)
+console.log(res1)
