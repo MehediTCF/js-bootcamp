9753d899157cf41801c216012eafa3bd5136c279
Mehedi Zamadar <meheditcf@gmail.com>
2020-01-06 23:46:02 +0600

    objects #2

---
+++ objects/objects-functions.js
@@ -0,0 +1,33 @@
+let myBook = {
+    title: '1501',
+    author: 'George',
+    pageCount: 15
+}
+
+let otherBook = {
+    title: 'Hye',
+    author: 'George S',
+    pageCount: 150
+}
+
+let getBook = function (book) {
+    console.log(`${book.title} By ${book.author}`)
+
+}
+
+getBook(myBook)
+getBook(otherBook)
+
+//
+
+let myAccount = {
+    name: 'Mehedi',
+    income: 100
+}
+
+let addIncome = function (account, amount) {
+    account.income = account.income + amount
+    console.log(account)
+}
+
+addIncome(myAccount, 2.5)
