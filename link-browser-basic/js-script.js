7a460a1f3a06b4034609b03a4dd359225733f4e1
Mehedi Zamadar <meheditcf@gmail.com>
2020-01-15 17:23:04 +0600

    advanced queries #1

--- link-browser-basic/js-script.js
+++ link-browser-basic/js-script.js
@@ -15,3 +15,24 @@ hs.forEach(function (p) {
 let newP = document.createElement('h1')
 newP.textContent = 'This is it!'
 document.querySelector('body').appendChild(newP)
+
+document.querySelectorAll('button')[1].addEventListener('click', function () {
+    console.log('Deleted!')
+})
+
+document.querySelector('#edit').addEventListener('click', function () {
+    console.log("Edited!")
+})
+
+document.querySelector('#remove').addEventListener('click', function () {
+    document.querySelectorAll('.note').forEach(function (note) {
+        note.remove()
+    })
+})
+
+
+document.querySelector('#removeh3').addEventListener('click', function () {
+    document.querySelectorAll('h3.tin').forEach(function (note) {
+        note.remove()
+    })
+})
