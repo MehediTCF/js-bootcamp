d360dbdf8bf20322fd3cb07f0a49ba4896785da4
Mehedi Zamadar <meheditcf@gmail.com>
2020-01-16 12:29:40 +0600

    searching with input text

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,31 @@
+const notes = [{
+    title: 'One',
+    body: 'One is important'
+
+}. {
+    title: 'Two',
+    body: 'Two is important'
+
+}, {
+    title: 'Three',
+    body: 'Three is important'
+
+}]
+
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+
+    })
+    console.log(filteredNotes)
+}
+
+renderNotes(notes, filters)
+
 
 //printing each para ******
 let ps = document.querySelectorAll('p')
@@ -10,3 +38,12 @@ let newPara = document.createElement('p')
 
 newPara.textContent = 'This is everything'
 document.querySelector('body').appendChild(newPara)
+
+
+document.querySelector("#search-note").addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+
+
+
+})
