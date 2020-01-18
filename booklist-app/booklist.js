cc9332cbb3f78255664ab83eff1150ca0ea3b3a3
Mehedi Zamadar <meheditcf@gmail.com>
2020-01-18 20:59:50 +0600

    mandatory commit

--- booklist-app/booklist.js
+++ booklist-app/booklist.js
@@ -3,25 +3,25 @@
 //     ss.textContent = e.target.value
 //     // console.log(e.target.value)
 // })
+// not working app
+let addbook = function () {
+    document.querySelectorAll('#bookname, #author').addEventListener('change', function (e) {
+        let booklist = document.querySelector('#booklist')
+        let row = document.createElement('tr')
+        row.innerHTML = `
+        <td></td>
+        <td>${e[0].target.value}</td>
+        <td>${e[0].target.value}</td>        `;
+
+        booklist.appendChild(row)
+    })
+
+}
 
 
-document.querySelectorAll('#bookname, #author').addEventListener('change', function (e) {
-    let booklist = document.querySelector('#booklist')
-    let row = document.createElement('tr')
-    row.innerHTML = `
-    <td></td>
-    <td>${e.target.value}</td>
-    <td>${e.target.value}</td>
-    `;
-    booklist.appendChild(row)
-    // booklist.textContent = e.target.value
-})
+document.querySelector('#submitform').addEventListener('click', addbook())
 
 
-document.querySelector('#author').addEventListener('input', function (e) {
-    let authorlist = document.querySelector('#authorlist')
-    authorlist.textContent = e.target.value
-})
 
 // document.querySelector('#submit').addEventListener('click', function () {
 //     document.querySelector('#bookname').addEventListener('input', function (e) {
